3683a332568686b0dac2a370c0ea6c0c61aa965c
tsmalla <tjark.smalla@gmx.de>
2018-06-18 10:36:03 +0200

    initially initialize the initial commit

---
+++ src/RadialChooser.js
@@ -0,0 +1,169 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight
+} from 'react-native';
+import Svg, {
+    Circle,
+    Ellipse,
+    G,
+    LinearGradient,
+    RadialGradient,
+    Line,
+    Path,
+    Polygon,
+    Polyline,
+    Rect,
+    Symbol,
+    SvgText,
+    Use,
+    Defs,
+    Stop
+} from 'react-native-svg';
+import React, {Component} from 'react';
+import {identity} from "../basicfunctions"
+
+const BUTTON_RADIUS = 50;
+const RADIAL_CHOOSER_DIA = 120;
+
+const rcbStyles = StyleSheet.create({
+    th: {
+        borderRadius: BUTTON_RADIUS
+    },
+    container: {
+        width: RADIAL_CHOOSER_DIA,
+        height: RADIAL_CHOOSER_DIA,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    svgContainer: {transform: [{rotate: "-90deg"}]}
+});
+
+function layerSwitch(lastDeg, deg) {
+    const isInsideBounds = ({upper, lower}) => deg <= upper && deg >= lower;
+    const wasInsideBounds = ({upper, lower}) => lastDeg <= upper && lastDeg >= lower;
+    const leftArea = {lower: 340, upper: 360};
+    const rightArea = {lower: 0, upper: 20};
+
+    if (isInsideBounds(rightArea) && wasInsideBounds(leftArea))
+        return 1;
+    else if (wasInsideBounds(rightArea) && isInsideBounds(leftArea))
+        return -1;
+    else return 0;
+}
+
+function LoadingRing({backgroundColor, tintColor, innerRadius, outerRadius, progress}) {
+    const borderWidth = outerRadius - innerRadius;
+    const circleRadius = innerRadius + borderWidth / 2;
+    const outerDiameter = outerRadius * 2;
+    const stroke = 2 * Math.PI * circleRadius;
+
+    return (<Svg width={outerDiameter} height={outerDiameter}
+         viewBox={`0 0 ${outerDiameter} ${outerDiameter}`}>
+        <Circle cx={outerRadius} cy={outerRadius}
+                r={circleRadius}
+                fill={"none"} stroke={backgroundColor}
+                strokeWidth={borderWidth}/>
+
+        <Circle cx={outerRadius} cy={outerRadius}
+                r={circleRadius}
+                fill={"none"} stroke={tintColor}
+                strokeWidth={borderWidth}
+                strokeDasharray={stroke}
+                strokeDashoffset={stroke * (1 - progress)}/>
+    </Svg>);
+}
+
+type Props = {};
+
+//https://codepen.io/xgad/post/svg-radial-progress-meters
+export default class RadialChooser extends Component<Props> {
+
+    constructor(props) {
+        super(props);
+        this.longPressDelay = props.longPressDelay;
+        this.state = {
+            showTimeSlider: false,
+            touchState: {
+                lastGrant: null,
+                isSelected: false,
+                lastDeg: 0
+            }
+        };
+    }
+
+    resetTouch = () => {
+        this.state.touchState.lastGrant = null;
+        this.state.touchState.isSelected = false;
+        this.state.initialPos = null;
+        this.state.lastDeg = 0;
+    };
+
+    initTouchAfterLongPress = (evt) => {
+        this.setState(oldState => {
+            oldState.touchState.lastGrant = Date.now();
+            return oldState;
+        });
+        const pageX = evt.nativeEvent.pageX;
+        const pageY = evt.nativeEvent.pageY;
+        setTimeout(() => {
+                if (this.state.touchState.lastGrant
+                    && (Date.now() - this.state.touchState.lastGrant) <= (this.longPressDelay + 50)) {
+                    this.setState(oldState => {
+                        oldState.touchState.isSelected = true;
+                        oldState.initialPos = {
+                            x: pageX,
+                            y: pageY
+                        };
+                        return oldState;
+                    });
+                }
+            }, this.longPressDelay
+        )
+    };
+
+    updateRadialState = (evt) => {
+        const pageX = evt.nativeEvent.pageX;
+        const pageY = evt.nativeEvent.pageY;
+
+        if (this.state.touchState.isSelected)
+            this.setState(oldState => {
+                const diffX = pageX - oldState.initialPos.x;
+                const diffY = oldState.initialPos.y - pageY;
+                const rad = Math.atan2(diffY, diffX);
+                let deg = (rad * 180) / Math.PI;
+
+
+                deg = deg - 90;
+
+                if (diffX <= 0 && diffY >= 0) {
+                    deg = -(270 + (90 - deg))
+                }
+                deg = -deg;
+
+                this.props.onTimesRoundedChange(this.props.timesRounded + layerSwitch(this.state.touchState.lastDeg, deg));
+                this.props.onRingProgressChange(deg / 360);
+
+                oldState.touchState.lastDeg = deg;
+                return oldState;
+            });
+    };
+
+    render() {
+        return (
+            <View style={rcbStyles.container}
+                  onPress={identity}
+                  onStartShouldSetResponder={(evt) => true}
+                  onMoveShouldSetResponder={(evt) => true}
+                  onResponderGrant={this.initTouchAfterLongPress}
+                  onResponderReject={this.resetTouch}
+                  onResponderRelease={this.resetTouch}
+                  onResponderMove={this.updateRadialState}>
+                <View style={rcbStyles.svgContainer}>
+                    <LoadingRing {...this.props}/>
+                </View>
+            </View>
+        );
+    }
+}
